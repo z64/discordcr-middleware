622f60e89196378b3b3afcb6035d834117eb45dd
Zac Nowicki <zachnowicki@gmail.com>
2017-11-30 16:42:44 +0000

    Specs: let message helper accept custom content

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,10 +1,11 @@
 require "spec"
 require "../src/discordcr-middleware"
+require "../src/discordcr-middleware/middleware/*"
 
 Client = Discord::Client.new("Bot TOKEN")
 
-def message
-  message_json = %({"attachments": [], "tts": false, "embeds": [], "timestamp": "2017-11-29T14:41:51.808000+00:00", "mention_everyone": false, "id": "385440229341003776", "pinned": false, "edited_timestamp": null, "author": {"username": "z64", "discriminator": "2639", "id": "120571255635181568", "avatar": "862875f54a2ef6db022512ee0d3b8d20"}, "mention_roles": [], "content": "test", "channel_id": "246283902652645376", "mentions": [], "type": 0})
+def message(content = "")
+  message_json = %({"attachments": [], "tts": false, "embeds": [], "timestamp": "2017-11-29T14:41:51.808000+00:00", "mention_everyone": false, "id": "385440229341003776", "pinned": false, "edited_timestamp": null, "author": {"username": "z64", "discriminator": "2639", "id": "120571255635181568", "avatar": "862875f54a2ef6db022512ee0d3b8d20"}, "mention_roles": [], "content": "#{content}", "channel_id": "246283902652645376", "mentions": [], "type": 0})
   Discord::Message.from_json(message_json)
 end
 

e6808202577ab347d4818bab7d0aa864b5b75fb1
Zac Nowicki <zachnowicki@gmail.com>
2018-02-01 14:23:29 -0500

    Add a set of typical context attributes

--- src/discordcr-middleware/context.cr
+++ src/discordcr-middleware/context.cr
@@ -5,6 +5,14 @@ module Discord
 
     getter message : Message
 
+    getter state = {} of String => Nil | String | Int32 | Int64 | Float64 | Bool
+
+    property int : Int16 | Int32 | Int64 | Nil
+    property uint : UInt16 | UInt32 | UInt64 | Nil
+    property string : String?
+    property float : Float32 | Float64 | Nil
+    property bool : Bool?
+
     def initialize(@client : Client, @message : Message)
     end
   end

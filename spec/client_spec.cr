aac39f4758ca2663e3b990c2a126f1f518a84815
Zac Nowicki <zachnowicki@gmail.com>
2018-04-02 12:24:21 +0000

    Add a spec illustrating the bug fixed with v0.3.1
    Prior to v0.3.1, the macro that generated the stack-supporting event
    handlers would wrongly register an event handler that would run the
    entire stack multiple times. In the given example in this spec,
    times_called would have a value of four since two event handlers were
    created. Now, the instance variable that stored stacks is removed and we
    instead use the closure of the event handler to store the registered
    stack, so that only one event handler is created per-stack.

---
+++ spec/client_spec.cr
@@ -0,0 +1,24 @@
+require "./spec_helper"
+
+module Discord
+  describe Client do
+    context "with multiple event handlers" do
+      it "calls stacks and trailing blocks once per event" do
+        mw = FlagMiddleware.new
+        event = Discord::WebSocket::Packet.new(
+         0_i64,
+         0_i64,
+         IO::Memory.new(message.to_json),
+         "MESSAGE_CREATE")
+
+        times_called = 0
+        ::Client.on_message_create(mw) { times_called += 1; }
+        ::Client.on_message_create(mw) { times_called += 1; }
+        ::Client.inject(event)
+        sleep 0.1.seconds
+
+        times_called.should eq 2
+      end
+    end
+  end
+end

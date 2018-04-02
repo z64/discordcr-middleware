3581a5b3940b45cff899e3d0f4694fe2c587d106
Zac Nowicki <zachnowicki@gmail.com>
2018-04-02 12:31:32 +0000

    Fix formatting

--- spec/client_spec.cr
+++ spec/client_spec.cr
@@ -6,14 +6,14 @@ module Discord
       it "calls stacks and trailing blocks once per event" do
         mw = FlagMiddleware.new
         event = Discord::WebSocket::Packet.new(
-         0_i64,
-         0_i64,
-         IO::Memory.new(message.to_json),
-         "MESSAGE_CREATE")
+          0_i64,
+          0_i64,
+          IO::Memory.new(message.to_json),
+          "MESSAGE_CREATE")
 
         times_called = 0
-        ::Client.on_message_create(mw) { times_called += 1; }
-        ::Client.on_message_create(mw) { times_called += 1; }
+        ::Client.on_message_create(mw) { times_called += 1 }
+        ::Client.on_message_create(mw) { times_called += 1 }
         ::Client.inject(event)
         sleep 0.1.seconds
 

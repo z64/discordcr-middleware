593ada6f438656918779716a2d0dbbec0c07b576
Zac Nowicki <zachnowicki@gmail.com>
2018-04-02 12:42:44 +0000

    Add raw message JSON because discordcr is broke

--- spec/client_spec.cr
+++ spec/client_spec.cr
@@ -5,10 +5,11 @@ module Discord
     context "with multiple event handlers" do
       it "calls stacks and trailing blocks once per event" do
         mw = FlagMiddleware.new
+        message_json = %({"attachments": [], "tts": false, "embeds": [], "timestamp": "2017-11-29T14:41:51.808000+00:00", "mention_everyone": false, "id": "385440229341003776", "pinned": false, "edited_timestamp": null, "author": {"username": "z64", "discriminator": "2639", "id": "123", "avatar": "862875f54a2ef6db022512ee0d3b8d20"}, "mention_roles": [], "content": "foo", "channel_id": "326472371441762304", "mentions": [], "type": 0})
         event = Discord::WebSocket::Packet.new(
           0_i64,
           0_i64,
-          IO::Memory.new(message.to_json),
+          IO::Memory.new(message_json),
           "MESSAGE_CREATE")
 
         times_called = 0
